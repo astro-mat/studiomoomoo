a29a8286d97fbff7f421456205cebbfc51242ac1
astro-mat <whoismatwarren@gmail.com>
2024-01-03 00:01:43 +0000

    replace form with google form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,3 +1,6 @@
 "<?php
 if($_POST["message"]) {
-    mail("whoismatwarren@gmail.com", "Here is the subject line",$_POST["insert your message here"]. "From: an@email.address");}?>" 
+    mail("whoismatwarren@gmail.com", "Here is the subject line",
+    $_POST["insert your message here"]. "From: an@email.address");
+    }
+    ?>" 
